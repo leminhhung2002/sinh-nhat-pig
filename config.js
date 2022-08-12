4981003972fbeff46198fbae91338068351736f2
leminhhung2002 <96687620+leminhhung2002@users.noreply.github.com>
2022-08-12 21:21:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'PIG';
 const giftUrl = '';
 const eventName = 'Happy Birthday ';
 const titleCard = 'Tặng PIG ';
-const contentCard = 'Chúc PIG sinh nhật vui vẻ nhé,chúc PIG luôn luôn hạnh phúc ngày nào cũng như ngày sinh nhật,luôn luôn giỏi giang ,luôn luôn xinh gái ,lúc nào cũng nở nụ cười trên môi á , Spiderman luôn bảo vệ trái đất còn tôi sẽ bảo về PIG vì PIG là thế giới của tôi hì hì ,luôn luôn nở nụ cười nha cho dù có chuyện gì á vì có spiderman ở đây rùi  và  đặc biệt lúc nào cũng đáng yêu như bài nhạc này nha hihi ❤️ ';
+const contentCard = '28/8 Là ngày mà một thiên thần sinh ra , mà thiên thần đó là pig .Chúc pig sinh nhật vui vẻ nhé,chúc pig luôn luôn hạnh phúc ngày nào cũng như ngày sinh nhật,luôn luôn giỏi giang ,luôn luôn xinh gái ,lúc nào cũng nở nụ cười trên môi á , Spiderman luôn bảo vệ trái đất còn tôi sẽ bảo về PIG vì PIG là thế giới của tôi hì hì ,luôn luôn nở nụ cười nha cho dù có chuyện gì á vì có spiderman ở đây rùi  và  đặc biệt lúc nào cũng đáng yêu như bài nhạc này nha hihi ❤️ ';
 
 const giftImage = 'lan.jpg';
 const base64 = '';
